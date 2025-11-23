6350e853b627333cc715c6bfdf0f425e0c4ed76b
allisont <allison_talla@yahoo.com>
2025-11-23 13:05:58 -0500

    Change layout of Login.jsx and create Login.css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,9 @@
 import {Link} from "react-router-dom";
 import React, { useState } from 'react';
 import axios from 'axios'
+
+import "../styles/Login.css"
+
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -18,50 +21,60 @@ const Login = () => {
         .catch((err) => alert('Error in Login'))
 }    
     return (
-        <div
-        align="center"> 
-            <h1>Login Page</h1>
-            <div>
-                <form style={{display: 'inline-block', textAlign: 'left'}}>
-                    <div>
-                        <label
-                            htmlFor="username">Username:
-                        </label><br/>
-                        <input
+        <div className="container"> 
+            <div className="image-sidebar">
+                Image goes here
+            </div>
+            <div className="main-content">
+                <div className="introduction">
+                    <p>
+                        Enter information about receipt scanner.
+                    </p>
+                </div>
+                <div className="form">
+                    <form>
+                        <div className="form-field">
+                            <label htmlFor="username">Username</label><br/>
+                            <input
                             type="text"
                             value={username}
                             id = "username"
                             name="username"
                             autoComplete="username"
                             onChange={(e) => setUsername(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <label
-                        htmlFor="password">Password:
-                        </label><br/>
-                        <input
+                            />
+                        </div>
+                        <div className="form-field">
+                            <label htmlFor="password">Password:</label><br/>
+                            <input
                             type={showPass? "text": "password"}
                             id="password"
                             value={password}
                             name="password"
                             autoComplete="password"
                             onChange={(e) => setPassword(e.target.value)}
-                        />
-                        <input type = "checkbox" 
-                        onChange = {() => setShowPass(v => !v)}
-                        style = {{marginLeft : 8}}/> -- Show Password
-                    </div> <br/> 
-                    <button
-                        type="button" onClick={(event) => {
-                        handleLogin(event, username, password)}}>
-                        Submit
-                    </button><br/>
-                    <Link
-                        to = "/Signup">
-                        Signup
-                    </Link>
-                </form>
+                            />
+                        </div>
+                        <div className="form-checkbox">
+                            <input type = "checkbox" 
+                            onChange = {() => setShowPass(v => !v)}
+                            style = {{marginLeft : 8}}/> -- Show Password
+                        </div>
+                        <div className='submit-button'>
+                            <button
+                                type="button" onClick={(event) => {
+                                handleLogin(event, username, password)}}>
+                                Submit
+                            </button><br/>
+                        </div>
+                        <div className="link">
+                            <Link
+                                to = "/Signup">
+                                Signup
+                            </Link>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     );
